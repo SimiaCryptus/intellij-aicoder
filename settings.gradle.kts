8dfcf1c7ee0bb2384a4b56afe6383b2df27e5b13
Andrew Charneski <acharneski@gmail.com>
2023-10-15 19:33:51 -0400

    wip

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version ("0.4.0")
 }
 
-includeBuild("../joe-penai/")
-includeBuild("../SkyeNet/")
+//includeBuild("../joe-penai/")
+//includeBuild("../SkyeNet/")
 //includeBuild("../AwsAgent/")
 
 
