08d9f022a4b900f9aa7759e94fcc037211aa1f32
Andrew Charneski <acharneski@gmail.com>
2023-11-25 22:50:05 -0500

    internal coder prototype

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,12 +25,13 @@ repositories {
 val kotlin_version = "1.9.20"
 val jetty_version = "11.0.18"
 val slf4j_version = "2.0.9"
-val skyenet_version = "1.0.39"
+val skyenet_version = "1.0.40"
 dependencies {
 
     implementation(group = "com.simiacryptus", name = "jo-penai", version = "1.0.36")
 
     implementation(group = "com.simiacryptus.skyenet", name = "core", version = skyenet_version)
+    implementation(group = "com.simiacryptus.skyenet", name = "kotlin", version = skyenet_version)
     implementation(group = "com.simiacryptus.skyenet", name = "webui", version = skyenet_version)
 
     implementation(group = "org.apache.httpcomponents.client5", name = "httpclient5", version = "5.2.1")
