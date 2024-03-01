c54db6d8ed34f471f78a57efaa63d262a5fadc74
Andrew Charneski <acharneski@gmail.com>
2024-02-29 22:46:44 -0500

    1.2.28 (#133)

--- src/main/kotlin/com/github/simiacryptus/aicoder/actions/generic/LineFilterChatAction.kt
+++ src/main/kotlin/com/github/simiacryptus/aicoder/actions/generic/LineFilterChatAction.kt
@@ -4,12 +4,9 @@ import com.github.simiacryptus.aicoder.ApplicationEvents
 import com.github.simiacryptus.aicoder.actions.BaseAction
 import com.github.simiacryptus.aicoder.actions.dev.AppServer
 import com.github.simiacryptus.aicoder.config.AppSettingsState
-import com.github.simiacryptus.aicoder.util.CodeChatSocketManager
 import com.github.simiacryptus.aicoder.util.ComputerLanguage
-import com.github.simiacryptus.aicoder.util.addApplyDiffLinks
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.simiacryptus.skyenet.core.platform.ApplicationServices
 import com.simiacryptus.skyenet.core.platform.Session
@@ -17,65 +14,76 @@ import com.simiacryptus.skyenet.core.platform.StorageInterface
 import com.simiacryptus.skyenet.core.platform.User
 import com.simiacryptus.skyenet.webui.application.ApplicationServer
 import com.simiacryptus.skyenet.webui.chat.ChatServer
+import com.simiacryptus.skyenet.webui.chat.ChatSocketManager
 import com.simiacryptus.skyenet.webui.session.SocketManager
 import com.simiacryptus.skyenet.webui.util.MarkdownUtil.renderMarkdown
 import org.slf4j.LoggerFactory
 import java.awt.Desktop
 import java.io.File
 
-class DiffChatAction : BaseAction() {
+class LineFilterChatAction : BaseAction() {
 
-  val path = "/diffChat"
+  val path = "/codeChat"
 
   override fun handle(e: AnActionEvent) {
-    val editor = e.getData(CommonDataKeys.EDITOR) ?: return
+      val editor = e.getData(CommonDataKeys.EDITOR) ?: return
     val session = StorageInterface.newGlobalID()
     val language = ComputerLanguage.getComputerLanguage(e)?.name ?: return
-    val document = editor.document
-    val filename = FileDocumentManager.getInstance().getFile(document)?.name ?: return
-    val primaryCaret = editor.caretModel.primaryCaret
-    val rawText: String
-    val selectionStart : Int
-    val selectionEnd : Int
-    val selectedText = primaryCaret.selectedText
-    if (null != selectedText) {
-      rawText = selectedText
-      selectionStart = primaryCaret.selectionStart
-      selectionEnd = primaryCaret.selectionEnd
-    } else {
-      rawText = document.text
-      selectionStart = 0
-      selectionEnd = rawText.length
+    val filename = FileDocumentManager.getInstance().getFile(editor.document)?.name ?: return
+    val code = editor.caretModel.primaryCaret.selectedText ?: editor.document.text
+    val lines = code.split("\n").toTypedArray()
+    val codelines = lines.withIndex().joinToString("\n") { (i, line) ->
+      "${i.toString().padStart(3, '0')} $line"
     }
-    val numberedText = rawText.split("\n")
-      .mapIndexed { lineNumber: Int, lineText: String ->
-        lineText
-//        String.format("%4d: %s", lineNumber + 1, lineText)
-      }.joinToString("\n")
-    agents[session] = object : CodeChatSocketManager(
+    agents[session] = object : ChatSocketManager(
       session = session,
-      language = language,
-      codeSelection = numberedText,
-      filename = filename,
-      api = api,
       model = AppSettingsState.instance.defaultChatModel(),
-      storage = ApplicationServices.dataStorageFactory(root)
+      userInterfacePrompt = """
+        |# `$filename`
+        |
+        |```$language
+        |$code
+        |```
+        """.trimMargin().trim(),
+      systemPrompt = """
+        |You are a helpful AI that helps people with coding.
+        |
+        |You will be answering questions about the following code located in `$filename`:
+        |
+        |```$language
+        |$codelines
+        |```
+        |
+        |Responses may use markdown formatting. Lines from the prompt can be included by using the line number in a response line (e.g. `\nLINE\n`).
+        |
+        |For example:
+        |
+        |```text
+        |001
+        |## Injected subtitle
+        |
+        |025
+        |026
+        |
+        |013
+        |014
+        |```
+        """.trimMargin(),
+      api = api,
+      applicationClass = ApplicationServer::class.java,
+      storage = ApplicationServices.dataStorageFactory(root),
     ) {
-      override val systemPrompt: String
-        get() = super.systemPrompt + """
-          Provide code patches in diff format within ```diff code blocks.
-          The diff format should use + for line additions, - for line deletions.
-          The diff should include sufficient context before every change to identify the location.
-        """.trimIndent()
+      override fun canWrite(user: User?): Boolean = true
       override fun renderResponse(response: String): String {
-        val withLinks = addApplyDiffLinks(rawText, response) { newCode ->
-          WriteCommandAction.runWriteCommandAction(e.project) {
-            document.replaceString(selectionStart, selectionEnd, newCode)
+        return renderMarkdown(response.split("\n").joinToString("\n") {
+          when {
+            // Is numeric, use line if in range
+            it.toIntOrNull()?.let { i -> lines.indices.contains(i) } == true -> lines[it.toInt()]
+            // Otherwise, use response
+            else -> it
           }
         }
-        val html = renderMarkdown(withLinks)
-        return """<div>$html</div>"""
-      }
+)      }
     }
 
     val server = AppServer.getServer(e.project)
@@ -95,12 +103,12 @@ class DiffChatAction : BaseAction() {
   override fun isEnabled(event: AnActionEvent) = true
 
   companion object {
-    private val log = LoggerFactory.getLogger(DiffChatAction::class.java)
+    private val log = LoggerFactory.getLogger(LineFilterChatAction::class.java)
     private val agents = mutableMapOf<Session, SocketManager>()
     val root: File get() = File(ApplicationEvents.pluginHome, "code_chat")
     private fun initApp(server: AppServer, path: String): ChatServer {
       server.appRegistry[path]?.let { return it }
-      val socketServer = object : ApplicationServer("Code Chat", path) {
+      val socketServer = object : ApplicationServer(applicationName = "Code Chat", path = path) {
         override val singleInput = false
         override val stickyInput = true
         override fun newSession(user: User?, session: Session) = agents[session]!!
