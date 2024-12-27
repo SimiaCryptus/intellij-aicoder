104e56421b56b3f1cc19ec3ac40cc0f286ccfb52
Andrew Charneski <acharneski@gmail.com>
2024-12-27 14:09:22 -0500

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -203,24 +203,10 @@ repositories {
     }
 }
 
-// Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
 changelog {
     groups.empty()
     repositoryUrl = providers.gradleProperty("pluginRepositoryUrl")
 }
-
-// Configure Gradle Kover Plugin - read more: https://github.com/Kotlin/kotlinx-kover#configuration
-/*
-kover {
-    reports {
-        total {
-            xml {
-                onCheck = true
-            }
-        }
-    }
-}
-*/
 
 tasks {
     publishPlugin {
